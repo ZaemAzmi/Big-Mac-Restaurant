c2493706a4976c564e17a084244d920493d4462e
Affif <U2000850@siswa.um.edu.my>
2023-05-05 21:34:28 +0800

    bismillah

--- scripts/animate.js
+++ scripts/animate.js
@@ -8,3 +8,23 @@ $(document).ready(function() {
       });
     }, 10000);
   });
+
+  ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    // delay: 20000,
+    
+  });
+
+  ScrollReveal().reveal('.main-title, .section-title', { delay: 750, origin: 'left'});
+  ScrollReveal().reveal('.sec-01, .image', { delay: 1000, origin: 'bottom',viewFactor: 0.5  });
+  ScrollReveal().reveal('.text-box-right', { delay: 1100, origin: 'right' });
+  ScrollReveal().reveal('.sec-02, .image', { delay: 1000, origin: 'top',viewFactor: 0.5});
+  ScrollReveal().reveal('.text-box-left', { delay: 1000, origin: 'left' });
+  ScrollReveal().reveal('.sec-03', {
+    delay:7500,
+    origin: 'bottom',
+    viewFactor: 0.8,
+    
+  });
