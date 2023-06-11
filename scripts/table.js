9becc5963817e7488a4c10cb63a4d13ae2f29dc0
Amirul Asyraf <amirulasyraf1411@gmail.com>
2023-06-11 18:44:08 +0800

    Book.html -> TableSelection.html -> altPreorder.html

--- scripts/table.js
+++ scripts/table.js
@@ -2,3 +2,11 @@ function tableClicked(tableNumber) {
     alert("Table " + tableNumber + " clicked!");
     // You can perform additional actions here, such as redirecting to a reservation page
 }
+
+function previousClicked(){
+    location.href = 'book.html';
+}
+
+function confirmClicked(){
+    location.href = 'altPreorder.html';
+}
