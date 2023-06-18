dfce3763a65e80b2c59121f9fa61d437bf7f38ad
aleng <abdhalim2002@gmail.com>
2023-06-18 11:44:27 +0800

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -3,35 +3,20 @@ var password = document.querySelector("#enterPassword");
 var loginOption = document.querySelector("#loginAs");
 var form = document.querySelector("#loginForm");
 
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  var formData = new FormData();
-  formData.append("username", username.value);
-  formData.append("password", password.value);
-  formData.append("loginAs", loginOption.value);
-
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", "LoginVer.php", true);
-
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      var response = xhr.responseText;
-      if (response === "admin") {
+    if (username.value === "admin" && password.value === "admin123" && loginOption.value === "admin") {
         form.action = "Adacc.html";
         form.submit();
-      } else if (response === "user") {
+    } else if (username.value === "user" && password.value === "user" && loginOption.value === "user") {
         form.action = "Profile.html";
         form.submit();
-      } else {
-        alert("Wrong username or password.");
-      }
+    } else {
+        alert("Wrong username, password, or login credential");
     }
-  };
-
-  xhr.send(formData);
 });
 
 function updateLoginOption(select) {
-  document.getElementById('loginOption').value = select.value;
+    document.getElementById('loginOption').value = select.value;
 }
